a581bbdc0f17ad57626cf571939ac397a390ddec
HazemSerdah <61620915+001hazem@users.noreply.github.com>
2023-01-15 14:01:35 +0200

    Exericise 8 (List)

---
+++ exercise8.py
@@ -0,0 +1,29 @@
+list1 =[1,2,3,4,5]
+list2 =["hazem", "test", "test list", "test"]
+
+# print(*list1)
+# print(list1, sep='')
+# print(*list2)
+# print(list2, sep='')
+
+list2.insert(4, "x")
+list1.extend([6,7,8,9,10])
+
+# print(*list1)
+# print(list2, sep='')
+
+del list1[5]
+list1.pop(5)
+print(*list1)
+
+
+
+
+
+
+
+
+
+
+
+
